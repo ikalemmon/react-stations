5d4fba10dfbb8a9076aba9bd7212798bb7d04465
ikalemmon <nagatadaiki@MacBook-Pro-142.local>
2022-02-22 13:57:08 +0900

    5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export const App = () => {
     const [dogUrl, setDogUrl] = useState(
       "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"
     )
-
+    
   return (
     <div className="App">
       <header>初めてのReact</header>
@@ -23,6 +23,7 @@ export const App = () => {
       <div>
         <h2>犬の画像を表示していくよ</h2>
       </div>
+      <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")}>更新ボタン</button>
     </div>
   )
 }
