7b66fab71c5dee6533ded90a2668ff2594ff6c4a
ikalemmon <nagatadaiki@MacBook-Pro-142.local>
2022-02-22 13:31:19 +0900

    3

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ export const App = () => {
   return (
     <div className="App">
       <header>初めてのReact</header>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"></img>
       <div>
-        <h2>Hello, world!</h2>
+        <h2>犬の画像を表示していくよ！！</h2>
       </div>
     </div>
   )
