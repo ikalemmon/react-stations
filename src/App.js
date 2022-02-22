7b26846618bb5bbce501ad3f3c8b865495d25fba
ikalemmon <nagatadaiki@MacBook-Pro-142.local>
2022-02-22 13:36:45 +0900

    4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // DO NOT DELETE
 
+
+
 import * as React from 'react'
 import './App.css'
 
@@ -7,11 +9,18 @@ import './App.css'
  *
  * @type {React.FC}
  */
+function Dog(){
+  const [DogURL, setDogURL] = React.useState(
+    "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"
+  )
+}
+
+
 export const App = () => {
   return (
     <div className="App">
       <header>初めてのReact</header>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"></img>
+      <img src={DogURL}></img>
       <div>
         <h2>犬の画像を表示していくよ！！</h2>
       </div>
